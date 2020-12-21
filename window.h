8eb8701ce1068ed1d475a9c1317d3645eacb59aa
Warran Penner <warranpenner@gmail.com>
2020-12-21 01:41:20 -0700

    Created and added renderer, window, and main source files.

---
+++ window.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <SDL.h>
+#include <stdio.h>
+#include <sstream>
+
+class Window {
+public:
+	//Intializes internals
+	Window();
+
+	//Creates window
+	bool init();
+
+	//Handles window events
+	void handleEvent(SDL_Event& e);
+
+	//Focuses on window
+	void focus();
+
+	//Shows windows contents
+	void render();
+
+	//Deallocates internals
+	void free();
+
+	//Window dimensions
+	int getWidth();
+	int getHeight();
+
+	//Window focii
+	bool hasMouseFocus();
+	bool hasKeyboardFocus();
+	bool isMinimized();
+	bool isShown();
+
+private:
+	//Window data
+	SDL_Window* mWindow;
+	SDL_Renderer* mRenderer;
+	int mWindowID;
+	const int DEF_W_ = 640, DEF_H_ = 480;
+
+	//Window dimensions
+	int mWidth;
+	int mHeight;
+
+	//Window focus
+	bool mMouseFocus;
+	bool mKeyboardFocus;
+	bool mFullScreen;
+	bool mMinimized;
+	bool mShown;
+};
